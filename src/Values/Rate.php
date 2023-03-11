de39ec1f069e5dac55873dfedf8a706290bd07bb
UserKazun <kazuusjlo8@gmail.com>
2023-03-11 15:09:58 +0900

    change propety name

--- src/Values/Rate.php
+++ src/Values/Rate.php
@@ -7,18 +7,18 @@ class Rate
     // Values as of 2023/03/04 1:16 are used as a sample. $1 = 136 JPY
     const USD_RATE = 0.73;
 
-    // convertValue is allow up to 9 digits.
+    // inputJpy is allow up to 9 digits.
     const MAX_LENGTH = 9;
 
-    private int $convertValue;
+    private int $inputJpy;
 
-    public function __construct(int $convertValue)
+    public function __construct(int $inputJpy)
     {
-        $this->convertValue = $convertValue;
+        $this->inputJpy = $inputJpy;
     }
 
     public function value(): int
     {
-        return $this->convertValue;
+        return $this->inputJpy;
     }
 }
